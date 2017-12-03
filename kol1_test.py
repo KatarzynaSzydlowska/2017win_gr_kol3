1d1898aca03440cffef53a3fbcbcdd96221f8659
Katarzyna Szydlowska <kszydlowska11@gmail.com>
2017-12-03 12:06:45 +0100

    v2

--- kol1_test.py
+++ kol1_test.py
@@ -1,29 +1,62 @@
-#4poznanski
+# 4poznanski
 import unittest
 import kol1
 
-class MyTest(unittest.TestCase):
-    
-    #def setUp(self):
 
-    def test_Client(self):
-        client_name = "Nowak"
-        client = kol1.Client(client_name)
-        self.assertIsNotNone(client)   
+class MyTest(unittest.TestCase):
+    def setUp(self):
+        self.client = kol1.Client('Nowak')
+        self.client2 = kol1.Client('Kowal')
+        self.client2.input(100)
+        self.bank = kol1.Bank()
+        self.bank.addClient(self.client)
+        self.bank.addClient(self.client2)
+
+    def test_bank_init(self):
+        self.assertIsNotNone(self.bank)
 
     def test_client_name(self):
-        client_name = "Nowak"
-        client = kol1.Client(client_name)
-        self.assertEqual(client.name,"Nowak")
+        self.assertEquals(self.client.name, 'Nowak')
 
-    def test_Bank(self):
-        bank = kol1.Bank()
-        self.assertIsNotNone(bank)
-    
+    def test_client_default_cash(self):
+        self.assertEquals(self.client.cash, 0)
+
     def test_add_client(self):
-        bank = kol1.Bank()
-        client = kol1.Client("Nowak")
-        bank.addClient(client)
-        #assert len(bank.clients[client]) == 0, "not empty"
+        self.assertTrue(self.client2 in self.bank.clients)
 
+    def test_input(self):
+        self.client.input(100)
+        self.assertEqual(self.client.cash, 100)
 
+    def test_withdraw(self):
+        self.client2.withdraw(50)
+        self.assertEqual(self.client2.cash, 50)
+
+    def test_transfer(self):
+        self.bank.transfer(self.client2, self.client, 10)
+        self.assertEquals(self.client2.cash, 90)
+        self.assertEquals(self.client.cash, 10)
+
+    def test_input_wrong_value(self):
+        self.client2.input(-100)
+        self.assertNotEqual(self.client2.cash, 0)
+
+    def test_withdraw_wrong_value(self):
+        self.client2.withdraw(150)
+        self.assertNotEqual(self.client2.cash, -50)
+
+    def test_transfer_wrong_value(self):
+        self.bank.addClient(self.client2)
+        self.bank.transfer(self.client, self.client2, -10)
+        self.assertNotEquals(self.client.cash, 110)
+        self.assertNotEquals(self.client2.cash, -10)
+
+    def test_transfer_more_than_in_account(self):
+        self.bank.addClient(self.client2)
+        self.bank.transfer(self.client, self.client2, 1000)
+        self.assertNotEquals(self.client.cash, -900)
+        self.assertNotEquals(self.client2.cash, 1000)
+
+    def test_client_not_in_bank(self):
+        self.client3 = kol1.Client('Kowalski')
+        self.assertFalse(self.client3 in self.bank.clients)
